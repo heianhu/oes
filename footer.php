ebd72d3f7ba20bd14cfcea7c72a157bdfdb3cb59
Eel 蒋 <eeel777jiang@gmail.com>
2017-07-19 21:12:06 +0800

    修改footer 增加时间和 一言

--- footer.php
+++ footer.php
@@ -1,14 +1,32 @@
 <!-- Footer -->
 				<footer id="footer">
 					<ul class="icons">
-						<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-						<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-						<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
-						<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-						<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-						<li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+					<div id=time ></div>
+
+							
+						
 					</ul>
+					<?php 
+					 $content = file_get_contents('https://sslapi.hitokoto.cn/');
+					print json_decode($content)->{'hitokoto'};
+							?>	
 					<ul class="copyright">
+
 						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://baidu.com">翻江倒海</a></li>
 					</ul>
 				</footer>
+
+
+
+				<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/jquery.dropotron.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
+			<script>
+				document.getElementById('time').innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());setInterval("document.getElementById('time').innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());",1000);
+			</script> 
