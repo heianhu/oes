11104303476d43518bbbbfbcede7bbe768b54f98
Eel 蒋 <eeel777jiang@gmail.com>
2017-07-20 20:22:53 +0800

    删除 普通用户 header的试题库按钮

--- header.php
+++ header.php
@@ -37,23 +37,7 @@
                     <nav id="nav">
                         <ul>
                             <li><a href="stdtest.php">参加新测试</a></li>
-                            <li>
-                                <a href="http://www.zcth.cn/question">试题库</a>
-                                <ul>
-                                    <li><a href="left-sidebar.html">Left Sidebar</a></li>
-                                    <li><a href="right-sidebar.html">Right Sidebar</a></li>
-                                    <li><a href="no-sidebar.html">No Sidebar</a></li>
-                                    <li>
-                                        <a href="#">Submenu</a>
-                                        <ul>
-                                            <li><a href="#">Option 1</a></li>
-                                            <li><a href="#">Option 2</a></li>
-                                            <li><a href="#">Option 3</a></li>
-                                            <li><a href="#">Option 4</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
+                            
                             <li><a href="resumetest.php">恢复测试</a></li>
                             <li><a href="viewresult.php">察看结果</a></li>
                             <li><a href="editprofile.php?edit=edit'}" class="button special">个人</a>
